9869ff42769fa6a5697a3e86945dddc97f7f714d
zhanghao gogogo <2015074698@qq.com>
2024-04-07 03:14:02 +0000

    3

---
+++ src/main/java/org/Factore/Bridge/DrawAPI.java
@@ -0,0 +1,5 @@
+package org.Factore.Bridge;
+
+public interface DrawAPI {
+    public void drawCircle(int radius, int x, int y);
+ }
---
+++ src/main/java/org/Factore/Bridge/GreenCircle.java
@@ -0,0 +1,9 @@
+package org.Factore.Bridge;
+
+public class GreenCircle implements DrawAPI {
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+       System.out.println("Drawing Circle[ color: green, radius: "
+          + radius +", x: " +x+", "+ y +"]");
+    }
+ }
